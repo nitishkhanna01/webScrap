191a8e1cdfd133c3456e827e0c426cf86fb647da
Nitish Khanna <137228675+nitishkhanna01@users.noreply.github.com>
2023-12-07 12:36:50 +0530

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -4,4 +4,4 @@ sudo apt-get install -y chromium-browser
 sudo apt install chromium-chromedriver
 sudo apt install python3
 sudo apt install python3-pip
-sudo pip install -r requirements.text
+sudo pip install -r requirement.text
