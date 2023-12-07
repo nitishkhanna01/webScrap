ce6c58e15dc11050bb4326b5b19a5948450fd982
Nitish Khanna <137228675+nitishkhanna01@users.noreply.github.com>
2023-12-07 12:36:34 +0530

    Create Install.sh

---
+++ Install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt update ; sudo apt upgrade -y
+sudo apt-get install -y chromium-browser
+sudo apt install chromium-chromedriver
+sudo apt install python3
+sudo apt install python3-pip
+sudo pip install -r requirements.text
